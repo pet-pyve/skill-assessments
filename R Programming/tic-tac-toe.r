f435571b70dd816c019dc6fc6662da603b52f75b
Yve Nichols-Evans <z5309547@ad.unsw.edu.au>
2022-09-16 00:01:21 +1000

    create board & get input

--- R Programming/tic-tac-toe.r
+++ R Programming/tic-tac-toe.r
@@ -1,1 +1,28 @@
 # Creating Tic Tac Toe in R!
+
+#getting input from user func
+input <- function() {
+  if (interactive()) {
+    con <- stdin()
+  } else {
+    con <- "stdin"
+  }
+  cat("X or O? ")
+  symbol <- readLines(con = con, n = 1)
+}
+
+#creating the board
+board <- matrix(data=NA, nrow = 3, ncol = 3)
+
+#getting input from user
+symbol <- input()
+
+#check the input 
+if (symbol != 'X' & symbol != 'O') {
+  print("wrong!")
+} else {
+  print("right!")
+}
+
+
+
